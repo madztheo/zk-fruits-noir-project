d3166fd7397d390dae77bba06205d17584437559
guelowrd <>
2023-04-06 16:28:54 +0100

    some bug fix on tests

--- scripts/deployProofRegister.js
+++ scripts/deployProofRegister.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const hre = require("hardhat");
+
 
 async function main() {
   const ProofRegister = await hre.ethers.getContractFactory("ProofRegister");
-  const proofRegister = await ProofRegister.deploy();
+  const proofRegister = await ProofRegister.deploy(process.env.VERIFIER_ADDRESS);
   await proofRegister.deployed();
-  console.log("ProofRegister deployed to:", turboVerifier.address);
+  console.log("ProofRegister deployed to:", proofRegister.address);
 }
 
 main()
